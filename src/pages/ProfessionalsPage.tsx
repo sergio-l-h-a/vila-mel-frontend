807df87fd9111d7b29f3873019836e8e2c1ec32b
Francisco-Sergio <lanhousearte@outlook.com>
2026-08-02 14:23:33 -0300

    Atualizar URL

--- src/pages/ProfessionalsPage.tsx
+++ src/pages/ProfessionalsPage.tsx
@@ -6,7 +6,7 @@ export default function ProfessionalsPage() {
   const [professionals, setProfessionals] = useState<Professional[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/professionals")
+    fetch("https://vila-mel-backend.onrender.com/professionals")
       .then((res) => res.json())
       .then((data) => setProfessionals(data));
   }, []);
