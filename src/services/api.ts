1bde648f003340b20899fd90031e608ce4b7a1ac
Francisco-Sergio <lanhousearte@outlook.com>
2026-08-03 22:07:42 -0300

    fix: atualizando API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://vila-mel-backend.onrender.com/professionals",
+    baseURL: "https://vila-mel-backend.onrender.com",
 });
