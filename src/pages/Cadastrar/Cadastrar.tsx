010102affe56f62b8c530624b746bdec722e9063
Francisco-Sergio <lanhousearte@outlook.com>
2026-08-04 15:26:15 -0300

    fix: atualizando arquivo profissionalCard

--- src/pages/Cadastrar/Cadastrar.tsx
+++ src/pages/Cadastrar/Cadastrar.tsx
@@ -66,7 +66,7 @@ export default function Cadastrar() {
   };
 
   const handleImageChange = (e: any) => {
-    setImageFile(e.target.file);
+    setImageFile(e.target.file[0]);
   };
 
   const handleSubmit = async (e: any) => {
