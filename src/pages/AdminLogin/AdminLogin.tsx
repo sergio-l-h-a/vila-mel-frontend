8dfd611f469d223b164d472ee7923c617a550716
Francisco-Sergio <lanhousearte@outlook.com>
2026-08-06 15:27:20 -0300

    fix: atualizando rota-admin

--- src/pages/AdminLogin/AdminLogin.tsx
+++ src/pages/AdminLogin/AdminLogin.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { api, loginAdmin } from "../../services/api";
+import { loginAdmin } from "../../services/api";
 import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
