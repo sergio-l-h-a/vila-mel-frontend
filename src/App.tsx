52674191a71174996757d1fa2c0dd499964064e9
Francisco-Sergio <lanhousearte@outlook.com>
2026-08-09 10:51:43 -0300

    fix: atualizando createProfessional v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Sobre from "./pages/Sobre/Sobre";
-import Contato from "./pages/Contato/Contato";
-import Cadastrar from "./pages/Cadastrar/Cadastrar";
-import Login from "./pages/Login/Login";
-import Admin from "./pages/Admin/Admin";
+import { BrowserRouter} from "react-router-dom";
+
 import ProfessionalsPage from "./pages/Professional/ProfessionalsPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
